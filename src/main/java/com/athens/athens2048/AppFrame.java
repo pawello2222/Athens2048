c4c3b5e4641318559e419762bf16c094a1196e1d
Pawel Wiszenko <pawello2222@gmail.com>
2018-03-19 17:50:52 +0100

    Added main window

---
+++ src/main/java/com/athens/athens2048/AppFrame.java
@@ -0,0 +1,10 @@
+package com.athens.athens2048;
+
+import javax.swing.*;
+
+class AppFrame extends JFrame {
+    AppFrame() {
+        this.setSize(600, 600);
+        this.setVisible(true);
+    }
+}
