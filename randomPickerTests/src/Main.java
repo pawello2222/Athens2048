3cbc9229519325fb43bb2927a1c8a466d9656ce7
Antony Lopez <lpzantony@gmail.com>
2018-03-19 20:06:25 +0100

    Add almost empty files for testing RandomPicker

---
+++ randomPickerTests/src/Main.java
@@ -0,0 +1,10 @@
+package src;
+public class Main {
+
+	public static void main(String[] args) {
+		int[][] grid = new int[4][4];
+		RandomPicker picker =  new RandomPicker(2);
+		System.out.println("Hello there " + picker.getA());
+	}
+
+}
---
+++ randomPickerTests/src/RandomPicker.java
@@ -0,0 +1,12 @@
+package src;
+import java.util.Random;
+
+public class RandomPicker{
+	private int a;
+	public RandomPicker(int aValue){
+		a = aValue;
+	}
+	public int  getA(){
+		return a;
+	}
+}
